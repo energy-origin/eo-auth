c5848d22ceb6cda328d9b5b1e71563c9c8a72e2c
Jakob Kristensen <jakob@jakobk.dk>
2022-01-06 12:56:11 +0100

    Test log output as JSON (#52)

--- src/auth_api/app.py
+++ src/auth_api/app.py
@@ -21,6 +21,7 @@ from .endpoints import (
     InspectToken,
     CreateTestToken,
 )
+from .endpoints.test import TestLogging, TestLoggingException
 
 
 def create_app() -> Application:
@@ -32,6 +33,20 @@ def create_app() -> Application:
         name='Auth API',
         secret=INTERNAL_TOKEN_SECRET,
         health_check_path='/health',
+    )
+
+    # -- OpenID Connect Login ------------------------------------------------
+
+    app.add_endpoint(
+        method='GET',
+        path='/log/test',
+        endpoint=TestLogging(),
+    )
+
+    app.add_endpoint(
+        method='GET',
+        path='/log/test/exception',
+        endpoint=TestLoggingException(),
     )
 
     # -- OpenID Connect Login ------------------------------------------------
